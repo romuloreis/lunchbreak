bae45c3f166cf629a60f76d73b75b396c394cfa3
Rômulo Reis de Oliveira <romulo.reis.oliveira@gmail.com>
2022-02-16 11:56:45 -0300

    Update ValidationUtil.java

--- src/main/java/br/com/lunchbreak/lunchbreak/util/ValidationUtil.java
+++ src/main/java/br/com/lunchbreak/lunchbreak/util/ValidationUtil.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ValidationUtil {
     
-    private final static LocalTime DEFAULT_EXPIRED_TIME_VOTING = LocalTime.parse("23:55");
+    private final static LocalTime DEFAULT_EXPIRED_TIME_VOTING = LocalTime.parse("11:30");
 
     public boolean isVotingTimeExpired() {
         return LocalTime.now().isAfter(DEFAULT_EXPIRED_TIME_VOTING);
